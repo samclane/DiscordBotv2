976958c82729b9f9b6356564cd0cf288dffed969
Sawyer McLane <samclane@gmail.com>
2024-04-15 09:33:56 -0600

    Refactor voice_cog.py and main.py

---
+++ cogs/voice_cog.py
@@ -0,0 +1,90 @@
+import asyncio
+from io import BytesIO
+import discord
+from discord.ext import commands
+from aiogtts import aiogTTS
+import aiohttp
+import os
+from audiofix import FFmpegPCMAudio
+
+
+class VoiceCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.aiogtts = aiogTTS()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ):
+        if member == self.bot.user or member.bot:
+            return
+
+        # Leaving a voice channel or going afk
+        if before.channel is not None:
+            if after.channel == before.channel.guild.afk_channel:
+                await self.afk_user(member, before.channel)
+            else:
+                await self.depart_user(member, before.channel)
+
+        # Joining a voice channel
+        if after.channel is not None:
+            await self.greet_user(member)
+
+    async def greet_user(self, member: discord.Member):
+        await self.say_line(f"{member.name} has joined.", member.voice.channel)
+
+    async def depart_user(self, member: discord.Member, channel: discord.VoiceChannel):
+        await self.say_line(f"{member.name} has left.", channel)
+
+    async def afk_user(self, member: discord.Member, channel: discord.VoiceChannel):
+        await self.say_line(f"{member.name} went afk", channel)
+
+    async def say_line(self, line: str, channel: discord.VoiceChannel):
+        buffer = BytesIO()
+
+        tts_url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}".format(
+            voice_id="EXAVITQu4vr4xnSDxMaL"
+        )
+        model_id = "eleven_turbo_v2"
+        formatted_message = {
+            "model_id": model_id,
+            "text": line,
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                tts_url,
+                headers={
+                    "Content-Type": "application/json",
+                    "xi-api-key": os.environ["XI_API_KEY"],
+                },
+                json=formatted_message,
+            ) as response:
+                if response.status == 200:
+                    buffer = BytesIO(await response.read())
+                else:
+                    print(
+                        "ElevenLabs Request failed with status code:", response.status
+                    )
+                    print("Response content:", await response.text())
+                    print("Falling back to aiogTTS")
+                    await self.aiogtts.write_to_fp(line, buffer)
+
+        buffer.seek(0)
+
+        voice_guild = channel.guild
+        if voice_guild is not None:
+            voice_channel = await channel.connect()
+
+            voice_channel.play(FFmpegPCMAudio(buffer.read(), pipe=True))
+
+            while voice_channel.is_playing():
+                await asyncio.sleep(1)
+
+            await voice_channel.disconnect()
+        else:
+            await channel.send("You need to join a voice channel first!")
--- main.py
+++ main.py
@@ -12,10 +12,11 @@ from audiofix import FFmpegPCMAudio
 import aiosqlite
 import aiohttp
 
+from cogs.voice_cog import VoiceCog
+
 MY_GUILD = discord.Object(id=int(os.environ["DISCORD_GUILD"]))
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-aiogtts = aiogTTS()
 
 
 class MyBot(commands.Bot):
@@ -28,6 +29,7 @@ class MyBot(commands.Bot):
         )
 
     async def setup_hook(self):
+        await self.add_cog(VoiceCog(self))
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
@@ -168,37 +170,6 @@ async def remove_role(
     )
 
 
-@client.event
-async def on_voice_state_update(
-    member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
-):
-    if member == client.user or member.bot:
-        return
-
-    # Leaving a voice channel or going afk
-    if before.channel is not None:
-        if after.channel == before.channel.guild.afk_channel:
-            await afk_user(member, before.channel)
-        else:
-            await depart_user(member, before.channel)
-
-    # Joining a voice channel
-    if after.channel is not None:
-        await greet_user(member)
-
-
-async def greet_user(member: discord.Member):
-    await say_line(f"{member.name} has joined.", member.voice.channel)
-
-
-async def depart_user(member: discord.Member, channel: discord.VoiceChannel):
-    await say_line(f"{member.name} has left.", channel)
-
-
-async def afk_user(member: discord.Member, channel: discord.VoiceChannel):
-    await say_line(f"{member.name} went afk", channel)
-
-
 @client.tree.command()
 async def restart(interaction: discord.Interaction):
     """Restarts the bot."""
@@ -210,7 +181,7 @@ async def restart(interaction: discord.Interaction):
         )
         return
 
-    await interaction.response.send_message("Restarting the bot...")
+    await interaction.response.send_message("Bot restarting, please wait.")
     await client.close()
 
 
@@ -263,50 +234,6 @@ async def print_whitelist(interaction: discord.Interaction):
     ]
     await interaction.response.send_message(f"Whitelist: {whitelist}")
 
-
-async def say_line(line: str, channel: discord.VoiceChannel):
-    buffer = BytesIO()
-
-    tts_url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}".format(
-        voice_id="EXAVITQu4vr4xnSDxMaL"
-    )
-    model_id = "eleven_turbo_v2"
-    formatted_message = {
-        "model_id": model_id,
-        "text": line,
-    }
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            tts_url,
-            headers={
-                "Content-Type": "application/json",
-                "xi-api-key": os.environ["XI_API_KEY"],
-            },
-            json=formatted_message,
-        ) as response:
-            if response.status == 200:
-                buffer = BytesIO(await response.read())
-            else:
-                print("ElevenLabs Request failed with status code:", response.status)
-                print("Response content:", await response.text())
-                print("Falling back to aiogTTS...")
-                await aiogtts.write_to_fp(line, buffer)
-
-    buffer.seek(0)
-
-    voice_guild = channel.guild
-    if voice_guild is not None:
-        voice_channel = await channel.connect()
-
-        voice_channel.play(FFmpegPCMAudio(buffer.read(), pipe=True))
-
-        while voice_channel.is_playing():
-            await asyncio.sleep(1)
-
-        await voice_channel.disconnect()
-    else:
-        await channel.send("You need to join a voice channel first!")
 
 
 # Add this function to create the whitelist table if it doesn't exist
