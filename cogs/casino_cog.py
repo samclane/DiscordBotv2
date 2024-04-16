66ac9042389ca4c403b775be2a97e2d6e67194c2
Sawyer McLane <samclane@gmail.com>
2024-04-16 17:50:38 -0600

    Removed weekly lotto

--- cogs/casino_cog.py
+++ cogs/casino_cog.py
@@ -1,28 +1,16 @@
-import random
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 from cogs.games.slots import SlotMachine
-
-JACKPOT_DEFAULT = 1000
 
 
 @app_commands.guild_only()
 class CasinoCog(commands.Cog):
     def __init__(self, bot):
         self.bot: discord.Client = bot
-        self.lottery_jackpot = JACKPOT_DEFAULT
         self.economy_cog = self.bot.get_cog("EconomyCog")
         self.slot_machine = SlotMachine.default()
-
-    async def cog_load(self) -> None:
-        self.lottery.start()
-        await super().cog_load()
-
-    async def cog_unload(self) -> None:
-        self.lottery.stop()
-        await super().cog_unload()
 
     @app_commands.command()
     async def slots(self, interaction: discord.Interaction, bet: int):
@@ -52,15 +40,3 @@ class CasinoCog(commands.Cog):
             await interaction.response.send_message(
                 f"{response}\nBetter luck next time! You lost {bet}."
             )
-
-    @app_commands.command()
-    async def lottery_jackpot(self, interaction: discord.Interaction):
-        """Prints the current jackpot."""
-        await interaction.response.send_message(f"Jackpot: {self.lottery_jackpot}")
-
-    @tasks.loop(hours=168)
-    async def lottery(self):
-        users = await self.economy_cog.get_registered_users()
-        winner = random.choice(users)
-        await self.economy_cog.deposit_money(winner, self.lottery_jackpot)
-        self.lottery_jackpot = JACKPOT_DEFAULT
